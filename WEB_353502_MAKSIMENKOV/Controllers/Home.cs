7679d387f95467ba697f98eb9341a2c42cfe9ac8
Vlad20055 <maksimenkovvlad111@mail.ru>
2025-09-05 00:54:18 +0300

    Добавьте файлы проекта.

---
+++ WEB_353502_MAKSIMENKOV/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_353502_MAKSIMENKOV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WEB_353502_MAKSIMENKOV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_353502_MAKSIMENKOV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
